a8fcf37a60e8c587b55e744ac405efed34e53656
DongGu Kang <ehdrnsla3020@naver.com>
2017-01-10 13:38:18 +0900

    Update WhatDate.java

--- WhatDate.java
+++ WhatDate.java
@@ -1,6 +1,9 @@
-package test;
+//A program that inputs date and outputs what day of the week.
+//You only need to pay attention to the formulas and processing codes that require a leap year.
 
+package test;
 import java.util.*;
+
 class WhatDate{
 	public static void main(String[] args){
 		int Total_Days=1,sw=0;
@@ -52,27 +55,26 @@ class WhatDate{
 			break;
 		}
 		Total_Days+=((year-1900)*365) +((year-1900)/4) + days;
-		//Step 3,5
-		if((year-1900)%4==0 && (month==1 || month==2))
+		
+		if((year-1900)%4==0 && (month==1 || month==2))   //leap year
 			Total_Days-=1;
-		//Step 4
+	
 		System.out.println(Total_Days);
 		switch(Total_Days%7){
-		case 0 : System.out.println
-		(year+"/"+month+"/"+days+" = "+"Saturday");break;
-		case 1 : System.out.println
-		(year+"/"+month+"/"+days+" = "+"Sunday");break;
-		case 2 : System.out.println
-		(year+"/"+month+"/"+days+" = "+"Monday");break;
-		case 3 : System.out.println
-		(year+"/"+month+"/"+days+" = "+"Tuesday");break;
-		case 4 : System.out.println
-		(year+"/"+month+"/"+days+" = "+"Wednesday");break;
-		case 5 : System.out.println
-		(year+"/"+month+"/"+days+" = "+"Thursday");break;
-		case 6 : System.out.println
-		(year+"/"+month+"/"+days+" = "+"Friday");break;
-		//Step 6
+			case 0 : System.out.println
+			(year+"/"+month+"/"+days+" = "+"Saturday");break;
+			case 1 : System.out.println
+			(year+"/"+month+"/"+days+" = "+"Sunday");break;
+			case 2 : System.out.println
+			(year+"/"+month+"/"+days+" = "+"Monday");break;
+			case 3 : System.out.println
+			(year+"/"+month+"/"+days+" = "+"Tuesday");break;
+			case 4 : System.out.println
+			(year+"/"+month+"/"+days+" = "+"Wednesday");break;
+			case 5 : System.out.println
+			(year+"/"+month+"/"+days+" = "+"Thursday");break;
+			case 6 : System.out.println
+			(year+"/"+month+"/"+days+" = "+"Friday");break;
 		}
 	}
 }
