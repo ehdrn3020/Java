064ea7c0442788acc86231eaeda56a7624f9a81b
DongGu Kang <ehdrnsla3020@naver.com>
2017-02-08 12:57:33 +0900

    Add files via upload

---
+++ Random_function.java
@@ -0,0 +1,20 @@
+package grammer;
+
+import java.util.Random;
+import java.util.Scanner;
+
+class Random_function {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt(); //A>B
+		int ranval;
+		Random ran = new Random();
+		
+		for(int i=0;i<10;i++){
+			ranval = ran.nextInt(A-B+1);
+			ranval += B;
+			System.out.println(ranval);
+		}
+	}
+}
